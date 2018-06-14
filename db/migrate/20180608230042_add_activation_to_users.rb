c179ecc68060a68f073ca552d95d9944a0738b75
haonguyenit232 <haonguyenit232@gmail.com>
2018-06-14 10:29:14 +0700

    xong

--- db/migrate/20180608230042_add_activation_to_users.rb
+++ db/migrate/20180608230042_add_activation_to_users.rb
@@ -3,7 +3,5 @@ class AddActivationToUsers < ActiveRecord::Migration[5.1]
     add_column :users, :activation_digest, :string
     add_column :users, :activated, :boolean, default: false
     add_column :users, :activated_at, :datetime
-    add_column :users, :reset_set_at, :datetime
-    add_column :users, :reset_digest, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -43,8 +43,8 @@ ActiveRecord::Schema.define(version: 20180613011045) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
-    t.datetime "reset_set_at"
     t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
