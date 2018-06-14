8d93753be12372cec6931936efb47d1c6c106b89
haonguyenit232 <haonguyenit232@gmail.com>
2018-06-14 18:43:50 +0700

    chapmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.delivery_method = :smtp
   host = "haohao232.herokuapp.com"
   config.action_mailer.default_url_options = { host: host, protocol: "https" }
@@ -78,7 +79,6 @@ Rails.application.configure do
     :authentication => "plain",
     :enable_starttls_auto => true
   }
-
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
